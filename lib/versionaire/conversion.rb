a0379788e9ed495a6e325d2675fa2a97ae796f00
Brooke Kuhlmann <brooke@alchemists.io>
2016-03-19 09:45:16 -0600

    Added conversion function.
    - Allows primitives to be converted into new versions.

---
+++ lib/versionaire/conversion.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# The gem namespace.
+module Versionaire
+  # Conversion function for casting (strict) a value into a version.
+  # rubocop:disable Style/MethodName
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def Version value
+    case value
+      when String
+        fail(Errors::Conversion, Errors::Conversion.string_message) unless value =~ Version.format
+        Version.new Version.arguments(*value.tr("v", "").split(".").map(&:to_i))
+      when Array
+        fail(Errors::Conversion, Errors::Conversion.array_message) unless (0..3).cover?(value.size)
+        Version.new Version.arguments(*value.fill(0, value.size..2))
+      when Hash
+        valid = value.keys.all? { |key| Version.keys.include? key }
+        fail(Errors::Conversion, Errors::Conversion.hash_message) unless valid
+        Version.new value
+      when Version then value
+      else fail(Errors::Conversion, Errors::Conversion.primitive_message)
+    end
+  end
+
+  module_function :Version
+end
---
+++ spec/lib/versionaire/conversion_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "Versionaire.Version" do
+  let(:version) { Versionaire::Version.new major: 1, minor: 2, maintenance: 3 }
+
+  describe ".Version" do
+    context "with string" do
+      it "converts version string" do
+        expect(Versionaire.Version("1.2.3")).to eq(version)
+      end
+
+      it "converts labeled version string" do
+        expect(Versionaire.Version("v1.2.3")).to eq(version)
+      end
+
+      it "fails with conversion error for invalid string" do
+        result = -> { Versionaire.Version "bogus" }
+        expect(&result).to raise_error(Versionaire::Errors::Conversion, /Invalid string conversion/)
+      end
+    end
+
+    context "with array" do
+      it "converts an empty array" do
+        expect(Versionaire.Version([])).to eq(Versionaire::Version.new)
+      end
+
+      it "converts an array with one argument" do
+        version = Versionaire::Version.new major: 1
+        expect(Versionaire.Version([1])).to eq(version)
+      end
+
+      it "converts an array with two arguments" do
+        version = Versionaire::Version.new major: 1, minor: 2
+        expect(Versionaire.Version([1, 2])).to eq(version)
+      end
+
+      it "converts an array with three arguments" do
+        expect(Versionaire.Version([1, 2, 3])).to eq(version)
+      end
+
+      it "fails with conversion error for array with more than three arguments" do
+        result = -> { Versionaire.Version [1, 2, 3, 4] }
+        expect(&result).to raise_error(Versionaire::Errors::Conversion, /Invalid array conversion/)
+      end
+    end
+
+    context "with hash" do
+      it "converts hash with all keys" do
+        expect(Versionaire.Version(major: 1, minor: 2, maintenance: 3)).to eq(version)
+      end
+
+      it "converts hash with partial keys" do
+        version = Versionaire::Version.new minor: 2
+        expect(Versionaire.Version(minor: 2)).to eq(version)
+      end
+
+      it "fails with conversion error for invalid hash" do
+        result = -> { Versionaire.Version maintenance: 5, bogus: "test" }
+        expect(&result).to raise_error(Versionaire::Errors::Conversion, /Invalid hash conversion/)
+      end
+    end
+
+    context "with version" do
+      it "returns version" do
+        expect(Versionaire.Version(version)).to eq(version)
+      end
+    end
+
+    context "with unsupported primitive" do
+      it "fails with conversion error" do
+        result = -> { Versionaire.Version 1 }
+        expect(&result).to raise_error(Versionaire::Errors::Conversion, /Invalid conversion/)
+      end
+    end
+
+    context "with unsupported object" do
+      let(:object) { double :object }
+
+      it "fails with conversion error" do
+        result = -> { Versionaire.Version object }
+        expect(&result).to raise_error(Versionaire::Errors::Conversion, /Invalid conversion/)
+      end
+    end
+  end
+end
