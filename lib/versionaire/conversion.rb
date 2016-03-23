e090ee2b51cac1e68105757ffa23b143022b1021
Brooke Kuhlmann <brooke@alchemists.io>
2016-03-22 21:32:32 -0600

    Refactored conversion function.
    - Improves readability and reduces code complexity.
    - The `VersionAid` is a private class meant for use by
      the `Version` function only.

--- lib/versionaire/conversion.rb
+++ lib/versionaire/conversion.rb
@@ -2,29 +2,57 @@
 
 # The gem namespace.
 module Versionaire
-  module_function
-
-  # Conversion function for casting (strict) a value into a version.
+  # Conversion function (strict) for casting an object into a version.
   # rubocop:disable Style/MethodName
-  # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def Version value
-    case value
-      when String
-        fail(Errors::Conversion, Errors::Conversion.string_message) unless value =~ Version.format
-        Version.new Version.arguments(*value.tr("v", "").split(".").map(&:to_i))
-      when Array
-        fail(Errors::Conversion, Errors::Conversion.array_message) unless (0..3).cover?(value.size)
-        Version.new Version.arguments(*value.fill(0, value.size..2))
-      when Hash
-        valid = value.keys.all? { |key| Version.keys.include? key }
-        fail(Errors::Conversion, Errors::Conversion.hash_message) unless valid
-        Version.new value
-      when Version
-        value
-      else
-        fail Errors::Conversion, Errors::Conversion.primitive_message
+  def Version object
+    aid = VersionAid.new object
+
+    case object
+      when String then aid.convert_from_string
+      when Array then aid.convert_from_array
+      when Hash then aid.convert_from_hash
+      when Version then object
+      else fail Errors::Conversion, Errors::Conversion.primitive_message
     end
   end
+  module_function :Version
+
+  # Aids with converting objects into valid versions.
+  class VersionAid
+    def initialize value
+      @value = value
+    end
+
+    def convert_from_string
+      fail(Errors::Conversion, Errors::Conversion.string_message) unless value =~ Version.format
+      Version.new string_to_arguments
+    end
+
+    def convert_from_array
+      fail(Errors::Conversion, Errors::Conversion.array_message) unless (0..3).cover?(value.size)
+      Version.new array_to_arguments
+    end
+
+    def convert_from_hash
+      fail(Errors::Conversion, Errors::Conversion.hash_message) unless required_keys?
+      Version.new value
+    end
+
+    private
+
+    attr_reader :value
+
+    def string_to_arguments
+      Version.arguments(*value.tr("v", "").split(".").map(&:to_i))
+    end
+
+    def array_to_arguments
+      Version.arguments(*value.fill(0, value.size..2))
+    end
+
+    def required_keys?
+      value.keys.all? { |key| Version.keys.include? key }
+    end
+  end
+  private_constant :VersionAid
 end
