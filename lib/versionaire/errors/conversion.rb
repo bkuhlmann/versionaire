2f837eda01df5072c283c84f8bcbf82a695da5d8
Brooke Kuhlmann <brooke@alchemists.io>
2016-11-01 20:16:32 -0600

    Removed deprecated conversion error message strings.
    - These are no longer used.
    - See Versionaire::Converter for details of error messages used.

--- lib/versionaire/errors/conversion.rb
+++ lib/versionaire/errors/conversion.rb
@@ -4,25 +4,6 @@ module Versionaire
   module Errors
     # Thrown when attempting to convert (cast) a primitive to a version.
     class Conversion < Base
-      def self.string_message
-        warn "[DEPRECATION]: .string_message is deprecated and will be removed in the next major release."
-        %(Invalid string conversion. Use: "<major>.<minor>.<maintenance>" or "v<major>.<minor>.<maintenance>".)
-      end
-
-      def self.array_message
-        warn "[DEPRECATION]: .array_message is deprecated and will be removed in the next major release."
-        "Invalid array conversion. Use: [], [<major>], [<major>, <minor>], or [<major>, <minor>, <maintenance>]."
-      end
-
-      def self.hash_message
-        warn "[DEPRECATION]: .array_message is deprecated and will be removed in the next major release."
-        "Invalid hash conversion. Use: {major: <major>, minor: <minor>, maintenance: <maintenance>}."
-      end
-
-      def self.primitive_message
-        warn "[DEPRECATION]: .primitive_message is deprecated and will be removed in the next major release."
-        "Invalid conversion. Use: String, Array, or Hash."
-      end
     end
   end
 end
