57aaa96b3edb17bf78e267952cd8f7e373782df3
Brooke Kuhlmann <brooke@alchemists.io>
2019-02-16 13:02:28 -0700

    Refactored converter to construct on an object.
    Improves readability/terminology by ensuring the constructor references
    an *object* as also used within the `Version` function.

--- lib/versionaire/conversion.rb
+++ lib/versionaire/conversion.rb
@@ -20,20 +20,20 @@ module Versionaire
 
   # Aids with converting objects into valid versions.
   class Converter
-    def initialize value
-      @value = value
+    def initialize object
+      @object = object
     end
 
     def from_string
       body = %(Use: "<major>.<minor>.<maintenance>" or "v<major>.<minor>.<maintenance>".)
-      fail(Errors::Conversion, error_message(value, body)) unless Version.regex.match? value
+      fail(Errors::Conversion, error_message(object, body)) unless Version.regex.match? object
 
       Version.new string_to_arguments
     end
 
     def from_array
       body = "Use: [], [<major>], [<major>, <minor>], or [<major>, <minor>, <maintenance>]."
-      fail(Errors::Conversion, error_message(value, body)) unless (0..3).cover?(value.size)
+      fail(Errors::Conversion, error_message(object, body)) unless (0..3).cover?(object.size)
 
       Version.new array_to_arguments
     end
@@ -42,29 +42,29 @@ module Versionaire
       body = "Use: {major: <major>}, " \
              "{major: <major>, minor: <minor>}, or " \
              "{major: <major>, minor: <minor>, maintenance: <maintenance>}."
-      fail(Errors::Conversion, error_message(value, body)) unless required_keys?
+      fail(Errors::Conversion, error_message(object, body)) unless required_keys?
 
-      Version.new value
+      Version.new object
     end
 
     def from_object
-      fail Errors::Conversion, error_message(value, "Use: String, Array, Hash, or Version.")
+      fail Errors::Conversion, error_message(object, "Use: String, Array, Hash, or Version.")
     end
 
     private
 
-    attr_reader :value
+    attr_reader :object
 
     def string_to_arguments
-      Version.arguments(*value.tr("v", "").split(".").map(&:to_i))
+      Version.arguments(*object.tr("v", "").split(".").map(&:to_i))
     end
 
     def array_to_arguments
-      Version.arguments(*value.dup.fill(0, value.size..2))
+      Version.arguments(*object.dup.fill(0, object.size..2))
     end
 
     def required_keys?
-      value.keys.all? { |key| VERSION_ATTRIBUTES.include? key }
+      object.keys.all? { |key| VERSION_ATTRIBUTES.include? key }
     end
 
     def error_message object, body
