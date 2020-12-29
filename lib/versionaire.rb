8a94f2eac50ccd80e054e8ea7fe3b36397821a54
Brooke Kuhlmann <brooke@alchemists.io>
2020-12-29 15:51:57 -0700

    Updated conversion as function file
    Necessary to give this file a name that better encapsulates the unique
    nature of the implementation.

--- lib/versionaire.rb
+++ lib/versionaire.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
+require "versionaire/identity"
 require "versionaire/errors/base"
 require "versionaire/errors/cast"
 require "versionaire/errors/invalid_number"
 require "versionaire/errors/negative_number"
-require "versionaire/conversion"
-require "versionaire/identity"
 require "versionaire/version"
+require "versionaire/function"
 require "versionaire/cast"
