5d058d3cbae0aed12ad4b13bb01a16faae5e1424
Brooke Kuhlmann <brooke@alchemists.io>
2021-01-19 19:27:57 -0700

    Added version release notes

--- lib/versionaire/identity.rb
+++ lib/versionaire/identity.rb
@@ -5,7 +5,7 @@ module Versionaire
   module Identity
     NAME = "versionaire"
     LABEL = "Versionaire"
-    VERSION = "9.0.0"
+    VERSION = "9.1.0"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
   end
 end
