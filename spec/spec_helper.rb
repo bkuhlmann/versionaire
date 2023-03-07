97486735b88105e3f3a0b7c2b7041d2a4fbc999d
Brooke Kuhlmann <brooke@alchemists.io>
2023-03-06 17:49:29 -0700

    Refactored Pathname require tree refinement to pass single argument
    Minor syntax cleanup that requires less effort to use.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ SPEC_ROOT = Pathname(__dir__).realpath.freeze
 
 using Refinements::Pathnames
 
-Pathname.require_tree SPEC_ROOT, "support/shared_contexts/**/*.rb"
+Pathname.require_tree SPEC_ROOT.join("support/shared_contexts")
 
 RSpec.configure do |config|
   config.color = true
