5710cf5a6f7e66248ffa926bd0f024a3fa668edc
Brooke Kuhlmann <brooke@alchemists.io>
2026-07-16 16:40:37 -0600

    Updated to SimpleCov 1.0.0
    link:https://github.com/simplecov-ruby/simplecov/blob/main/CHANGELOG.md#100-2026-07-12[Details].
    
    Milestone: minor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,15 +2,7 @@
 
 require "simplecov"
 
-unless ENV["COVERAGE"] == "no"
-  SimpleCov.start do
-    add_filter %r(^/spec/)
-    enable_coverage :branch
-    enable_coverage_for_eval
-    minimum_coverage_by_file line: 95, branch: 95
-  end
-end
-
+SimpleCov.start "strict" unless ENV["COVERAGE"] == "no"
 Bundler.require :tools
 
 require "versionaire"
