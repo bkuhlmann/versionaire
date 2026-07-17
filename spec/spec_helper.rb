2a7d54e03ad4b136033d7ebc3be80914cd588950
Brooke Kuhlmann <brooke@alchemists.io>
2026-07-17 07:36:42 -0600

    Fixed SimpleCov strict to be a symbol
    Didn't realize, at first, that you can use a symbol for this profile which is more elegant than using a string.
    
    Milestone: patch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 
 require "simplecov"
 
-SimpleCov.start "strict" unless ENV["COVERAGE"] == "no"
+SimpleCov.start :strict unless ENV["COVERAGE"] == "no"
 Bundler.require :tools
 
 require "versionaire"
